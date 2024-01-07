5f75c96d0f3284f9f7328a7d930ee1a3be473300
Taylor Anthony Betanzos Rosas <tbetanzos@unsa.edu.pe>
2024-01-06 21:08:20 -0500

    correccion de errores

--- lab10/cgi-bin/mariadbej03.pl
+++ lab10/cgi-bin/mariadbej03.pl
@@ -8,13 +8,13 @@ my $password='pweb1';
 my $dsn="DBI:mysql:database=pweb1;host=127.0.0.1";
 my $dbh= DBI->connect($dsn, $user, $password) or die("no se pudo conectar");
 my $vote=5000;
-my $puntaje=7
+my $puntaje=7;
 my $sth=$dbh->prepare("SELECT * FROM peliculas where Votos>? and Puntaje>?");
 
-$sth->execute($vote,$puntaje);
+$sth->execute($vote, $puntaje);
 while(my @row = $sth->fetchrow_array){
     print "@row\n";
 }
 
 $sth->finish;
-$dbh->disconnect;
+$dbh->disconnect;   
